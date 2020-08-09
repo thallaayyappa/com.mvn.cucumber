ff6d067215fc3f2511e44ebab5e4defc661322f6
thallaayyappa <thalla.ayyappa@gmail.com>
2020-08-09 10:32:59 -0400

    Update Arraylistpractice.java

--- src/test/java/testcases/Arraylistpractice.java
+++ src/test/java/testcases/Arraylistpractice.java
@@ -7,7 +7,7 @@ public class Arraylistpractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//updates made by other team memeber related to this test script
 		
 		ArrayList ar = new ArrayList();
 		ar.add("thalla");
